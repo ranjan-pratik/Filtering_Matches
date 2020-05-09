7e5fa0ec5e9c28c6a089e9adc2b5c475a2302f8d
ranjan-pratik <65066391+ranjan-pratik@users.noreply.github.com>
2020-05-09 15:41:21 +0530

    Create AlgorithmicTests.java
    Setting up the project

---
+++ src/test/java/org/pr/project/core/AlgorithmicTests.java
@@ -0,0 +1,13 @@
+package org.pr.project.core;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AlgorithmicTests {
+
+	@Test
+	public void testSetup() {
+		assertTrue(true);
+	}
+}
